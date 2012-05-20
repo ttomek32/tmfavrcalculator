ffb62288448e21cb6b25fbca2e1306e6a5d48d71
tfrancuz@mp.pl <tfrancuz@mp.pl@4ccd77e4-e1ad-0bc2-6ab5-686057a35c16>
2012-05-20 21:19:33 +0000

    Dodałem info o wybranym procesorze + zachowywanie ostatniej pozycji i rozmiarów okna. Program używa rejestru Windows lub pliku ini w systemie GNU/Linuks.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <QtGui/QApplication>
+#include <QSplashScreen>
 #include "maindialog.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //QPixmap pixmap(":/images/idom-splash.png");  //Przygotowane na ewentualny splash screen z reklamami
+    //QSplashScreen *splash = new QSplashScreen(pixmap, Qt::WindowStaysOnTopHint);
+    //splash->show();
+    //qApp->processEvents();
+
+    QCoreApplication::setOrganizationName("TMF");
+    QCoreApplication::setOrganizationDomain("http://mikrokontrolery.blogspot.com/");
+    QCoreApplication::setApplicationName("TMFAVRCalculator");
+
     MainDialog w;
     w.show();
-    
+
+
+//    delete splash;
+
     return a.exec();
 }
