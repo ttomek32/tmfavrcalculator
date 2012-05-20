5845d0cb8d976170b0685d81ab901ef9f7c9f63c
tfrancuz@mp.pl <tfrancuz@mp.pl@4ccd77e4-e1ad-0bc2-6ab5-686057a35c16>
2012-05-20 21:19:33 +0000

    Dodałem info o wybranym procesorze + zachowywanie ostatniej pozycji i rozmiarów okna. Program używa rejestru Windows lub pliku ini w systemie GNU/Linuks.

--- trunk/src/maindialog.cpp
+++ trunk/src/maindialog.cpp
@@ -1,3 +1,4 @@
+#include <QSettings>
 #include "maindialog.h"
 #include "ui_maindialog.h"
 
@@ -5,10 +6,22 @@ MainDialog::MainDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::MainDialog)
 {
+    QSettings appsettings;
+     appsettings.beginGroup("MainWindow");
+     resize(appsettings.value("size", QSize(640, 480)).toSize());   //restore main window size and position
+     move(appsettings.value("pos", QPoint(0, 0)).toPoint());
+     appsettings.endGroup();
+
     ui->setupUi(this);
 }
 
 MainDialog::~MainDialog()
 {
+    QSettings appsettings;
+     appsettings.beginGroup("MainWindow");     //Save mainwindow size and position
+     appsettings.setValue("size", size());
+     appsettings.setValue("pos", pos());
+     appsettings.endGroup();
+
     delete ui;
 }
