4f702e58a1f2a0b13c283802ced93f4f8cbf3cd6
tfrancuz@mp.pl <tfrancuz@mp.pl@4ccd77e4-e1ad-0bc2-6ab5-686057a35c16>
2012-05-20 22:05:17 +0000

    W zakładce setup można wybierać ścieżkę do AVRDude. W zakładce "Programowanie uproszczone" można wybierać ścieżkę i plik hex do programowania.

--- trunk/include/maindialog.h
+++ trunk/include/maindialog.h
@@ -14,7 +14,15 @@ class MainDialog : public QDialog
 public:
     explicit MainDialog(QWidget *parent = 0);
     ~MainDialog();
-    
+
+signals:
+ void SetAVRDudePath(QString);        //Ustaw œcie¿kê w zak³adce SetUp do AVRDude
+ void SetFLASHFile(QString);          //Ustaw œcie¿kê do pliku FLASH w zak³adce "Uproszczone programowanie"
+
+private slots:
+    void AVRDudeSetPath();            //Ustaw œcie¿kê do programu AVRDude
+    void OpenFLASHFileDlg();          //Ustaw œcie¿kê do pliku z FLASH
+
 private:
     Ui::MainDialog *ui;
 };
