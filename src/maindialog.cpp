99aafbaf11dec3017142429515c51cc517971480
tfrancuz@mp.pl <tfrancuz@mp.pl@4ccd77e4-e1ad-0bc2-6ab5-686057a35c16>
2012-05-20 22:05:17 +0000

    W zakładce setup można wybierać ścieżkę do AVRDude. W zakładce "Programowanie uproszczone" można wybierać ścieżkę i plik hex do programowania.

--- src/maindialog.cpp
+++ src/maindialog.cpp
@@ -1,4 +1,8 @@
 #include <QSettings>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QDir>
+
 #include "maindialog.h"
 #include "ui_maindialog.h"
 
@@ -13,7 +17,70 @@ MainDialog::MainDialog(QWidget *parent) :
      appsettings.endGroup();
 
     ui->setupUi(this);
+
+    QString path;
+     appsettings.beginGroup("MainWindow");
+     path=appsettings.value("AVRDudePath").toString();   //Odczytaj œcie¿kê do AVRDude (jeœli jest)
+     appsettings.endGroup();
+    emit SetAVRDudePath(path);                          //Uzupe³nij pole edycji œcie¿ki do AVRDude
+
 }
+
+void MainDialog::AVRDudeSetPath()
+{
+    QString path;
+
+    QSettings appsettings;                               //Odczytaj œcie¿kê do AVRDude (jeœli jest)
+     appsettings.beginGroup("MainWindow");
+     path=appsettings.value("AVRDudePath").toString();
+     emit SetAVRDudePath(path);                          //Uzupe³nij pole edycji œcie¿ki do AVRDude
+
+    QFileDialog dialog(this);
+    dialog.setFileMode(QFileDialog::DirectoryOnly);
+    dialog.setDirectory(path);
+    QStringList fileNames;
+    if (dialog.exec())    //Wyœwietl dialog wyboru œcie¿ki
+     {
+        fileNames = dialog.selectedFiles();
+        if(fileNames.isEmpty()==false)
+        {
+            path=fileNames.at(0);        //Odczytaj œcie¿kê
+            appsettings.setValue("AVRDudePath", path);
+            emit SetAVRDudePath(path);
+        }
+     }
+
+    appsettings.endGroup();  //Zapisz zmiany
+}
+
+void MainDialog::OpenFLASHFileDlg()
+{
+    QString path;
+
+    QSettings appsettings;                               //Odczytaj œcie¿kê do AVRDude (jeœli jest)
+     appsettings.beginGroup("MainWindow");
+     path=appsettings.value("FLASHFilePath").toString();
+     emit SetFLASHFile(path);                          //Uzupe³nij pole edycji œcie¿ki do AVRDude
+
+    QFileDialog dialog(this);
+    dialog.setFileMode(QFileDialog::AnyFile);
+    dialog.setNameFilter(tr("IntelHex (*.hex)"));
+    dialog.setDirectory(path);
+    QStringList fileNames;
+    if (dialog.exec())    //Wyœwietl dialog wyboru œcie¿ki
+     {
+        fileNames = dialog.selectedFiles();
+        if(fileNames.isEmpty()==false)
+        {
+            path=fileNames.at(0);        //Odczytaj œcie¿kê
+            appsettings.setValue("FLASHFilePath", path);
+            emit SetFLASHFile(path);
+        }
+     }
+
+    appsettings.endGroup();  //Zapisz zmiany
+}
+
 
 MainDialog::~MainDialog()
 {
