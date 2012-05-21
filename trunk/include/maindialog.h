237a4315fb093dc596fa93362d4429114165e1de
tfrancuz@mp.pl <tfrancuz@mp.pl@4ccd77e4-e1ad-0bc2-6ab5-686057a35c16>
2012-05-21 15:38:52 +0000

    Dodałem zapamiętywanie ostatnio wybranego pliku FLASH, uzupełnianie nazwy w zakładce zaawansowanej, zapamiętuje stan checkboxów w setup i blokuje niektóre zakładki w zależności od wybranego trybu.

--- trunk/include/maindialog.h
+++ trunk/include/maindialog.h
@@ -13,15 +13,24 @@ class MainDialog : public QDialog
     
 public:
     explicit MainDialog(QWidget *parent = 0);
+    void HideAdvancedTabs(bool);     //Ukryj zak³adki z opcjami zaawansowanymi
+
     ~MainDialog();
 
 signals:
  void SetAVRDudePath(QString);        //Ustaw œcie¿kê w zak³adce SetUp do AVRDude
  void SetFLASHFile(QString);          //Ustaw œcie¿kê do pliku FLASH w zak³adce "Uproszczone programowanie"
+ void SetSimplifierViewChBox(bool);   //Ustaw checkbox widoku uproszczonego w zak³adce Setup
+ void SetAVRDudeWindowOnError(bool);  //Ustaw checkbox wyœwietlania okna AVRD Dude w przypadku b³êdu
+ void SetAVRDudeCMDLine(bool);        //Ustaw checkbox wyœwietlania linii polecenia AVR Dude
 
 private slots:
     void AVRDudeSetPath();            //Ustaw œcie¿kê do programu AVRDude
     void OpenFLASHFileDlg();          //Ustaw œcie¿kê do pliku z FLASH
+    //Z okienka SetUp
+    void SetupShowSimplifiedView(int);     //Checkbox pokazuj¹cy tylko widok uproszczony
+    void SetupShowAVDDudeWindow(int);      //Checkbox - poka¿ okno AVRDude tylko jeœli wyst¹pi³ b³¹d
+    void SetupShowAVRDudeCmd(int);         //Poka¿ wygenerowane polecenie AVRDude
 
 private:
     Ui::MainDialog *ui;
