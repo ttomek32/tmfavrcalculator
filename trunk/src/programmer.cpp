11a14a2cab48dd951466702b0b2afbbdc9440525
michal.gazy@gmail.com <michal.gazy@gmail.com@4ccd77e4-e1ad-0bc2-6ab5-686057a35c16>
2012-05-22 19:39:48 +0000

    Parser pliku konfiguracyjnego avrdude.conf

--- trunk/src/programmer.cpp
+++ trunk/src/programmer.cpp
@@ -1,7 +1,12 @@
 #include "programmer.h"
 
+// Initializes new instance of Programmer object.
 Programmer::Programmer() { }
 
+// Initializes new instance of Programmer object.
+// param QString id - unique ID number of the programmer.
+// param QString description - description of the programmer.
+// param QString type - type of the programmer.
 Programmer::Programmer(QString id, QString description, QString type)
 {
     m_ID = id;
@@ -9,16 +14,19 @@ Programmer::Programmer(QString id, QString description, QString type)
     m_Type = type;
 }
 
+// Gets the programmers ID.
 QString Programmer::GetID()
 {
     return m_ID;
 }
 
+// Gets the programmers description.
 QString Programmer::GetDescription()
 {
     return m_Description;
 }
 
+// Gets the type of the programmer.
 QString Programmer::GetType()
 {
     return m_Type;
